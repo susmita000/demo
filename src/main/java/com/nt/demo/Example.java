0fce82d469a5a3b7062ce24921eee82dd8f86dda
susmita000 <susmitapanigrahi6@gmail.com>
2018-10-16 13:38:28 +0530

    modified return type

--- src/main/java/com/nt/demo/Example.java
+++ src/main/java/com/nt/demo/Example.java
@@ -3,10 +3,11 @@ package com.nt.demo;
 public class Example {
 	int i=10;
 	int j=20;
-	public void add(int i,int j) {
+	public int add(int i,int j) {
 		this.i=i;
 		this.j=j;
-		System.out.println(i+j);
+		return add(10, 20);
+		
 	}
 
 }
