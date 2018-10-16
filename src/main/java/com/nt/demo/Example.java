81a611e17b43438be4aaf25380c14002bd31c82d
susmita000 <41376295+susmita000@users.noreply.github.com>
2018-10-16 14:12:47 +0530

    Method added by dev2

--- src/main/java/com/nt/demo/Example.java
+++ src/main/java/com/nt/demo/Example.java
@@ -6,7 +6,15 @@ public class Example {
 	public int add(int i,int j) {
 		this.i=i;
 		this.j=j;
-		return add(100, 20);
+		int z=i+j;
+		return z;
+		
+	}
+public int sub(int i,int j) {
+		this.i=i;
+		this.j=j;
+		int z=i-j;
+		return z;
 		
 	}
 
